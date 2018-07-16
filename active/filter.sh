fb8aad5f1f34a1a67efdc5ee4f598c648b5ed397
adroitadorkhan <adroitadorkhan@gmail.com>
2018-07-16 12:55:17 -0400

    Fixing Errors.

--- active/filter.sh
+++ active/filter.sh
@@ -119,9 +119,7 @@ comm -23 "$hosts" "$whitelist" >> "$fresh"
 # Add IP and Uncomment if you want Hosts
 awk '$0="0.0.0.0 "$0' $fresh > $active
 
-cat $active | tr -d '\r' > $fresh
-
-# mv -f $active $fresh
+mv -f $active $fresh
 
 # Read Total Domain Number
 totaldomains=$(awk 'END{print NR}' $fresh)
